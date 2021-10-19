532439a9a0d397c1c3990610a85f074e87bae22b
Байрамгалин Саит <bayramgalin95@gmail.com>
2021-10-19 15:00:23 +0500

    modifine post and post-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface IPost {
+  Id?: number;
+  Title: string;
+  Text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  posts: IPost[] = [
+    {
+      Id: 1,
+      Title: '123',
+      Text: 'Проверка поста angular',
+    },
+    {
+      Id: 2,
+      Title: '123',
+      Text: 'Проверка поста angular',
+    },
+  ];
   title = 'test-app';
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IPost } from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
+  @Input() post: IPost | any;
+  @Input() test: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
