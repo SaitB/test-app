fd966c4404f38391462ac5020dcbb4b2d75ae15a
Байрамгалин Саит <bayramgalin95@gmail.com>
2021-10-21 12:55:48 +0500

    #element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 
 export interface IPost {
-  Id?: number;
-  Title: string;
-  Text: string;
+	Id?: number;
+	Title: string;
+	Text: string;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  posts: IPost[] = [
-    {
-      Id: 1,
-      Title: '123',
-      Text: 'Проверка поста angular',
-    },
-    {
-      Id: 2,
-      Title: '123',
-      Text: 'Проверка поста angular',
-    },
-  ];
-  title = 'test-app';
-  constructor() {}
+	title = 'test-app';
+	isTest = false;
+	valueText = this.isTest ? 2 : 3;
 
-  ngOnInit(): void {}
+	posts: IPost[] = [
+		{
+			Id: 1,
+			Title: '123',
+			Text: 'Проверка поста angular',
+		},
+		{
+			Id: 2,
+			Title: '123',
+			Text: 'Проверка поста angular',
+		},
+	];
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	onAdd($event: IPost) {
+		this.posts.push($event);
+	}
 }
--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { IPost } from '../app.component';
 
 @Component({
-  selector: 'app-post-form',
-  templateUrl: './post-form.component.html',
-  styleUrls: ['./post-form.component.scss'],
+	selector: 'app-post-form',
+	templateUrl: './post-form.component.html',
+	styleUrls: ['./post-form.component.scss'],
 })
 export class PostFormComponent implements OnInit {
-  constructor() {}
+	@Output() onAdd = new EventEmitter();
+	title: string = '';
+	text: string = '';
+	post: IPost | any;
+	constructor() {}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {}
 
-  addPost() {}
+	addPost() {
+		if (this.title.trim() && this.text.trim()) {
+			this.post = {
+				Title: this.title,
+				Text: this.text,
+			};
+			this.onAdd.emit(this.post);
+		}
+	}
 }
